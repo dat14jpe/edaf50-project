c8f6f47aeec30d7134772e72e20240b3b18d6c4b
dat14jpe <dat14jpe@student.lu.se>
2018-03-28 22:33:38 +0200

    makefile and main files

---
+++ server_disk.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "On-disk server." << std::endl;
+    return 0;
+}
---
+++ server_mem.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "In-memory server." << std::endl;
+    return 0;
+}
