53f6cafdfaa51a9f3db8f858645606bf58d22b32
dat14mak <dat14mak@student.lu.se>
2018-03-29 10:11:55 +0200

    database.h

--- database.h
+++ database.h
@@ -1,5 +1,29 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <string>
+#include <vector>
+#include "article.h"
+#include "newsgroup.h"
 
 /* Database Interface */
 class Database {
-  
-}
+public:
+    // lists newsgourps;
+    virtual std::vector<std::string> list();
+    // creates a new newsgroup
+    virtual bool create(newsgroup ng);
+    // removes a newsgroup and it's articles
+    virtual bool remove(unsigned int ngId);
+    // lists articles in a newsgroup
+    virtual std::vector<Article> list(unsigned int ngId);
+    // reads an article
+    virtual std::string read(unsigned int ngId,unsigned int aId);
+    // writes to an article
+    virtual bool write(unsigned int ngId,unsigned int aId, std::string text);
+    // removes an article
+    virtual bool remove(unsigned int ngId, unsigned int aId);
+private:
+
+};
+#endif
